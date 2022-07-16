3ab8333dfd623a97e28372e18c6d604871c36dc5
Yong Jin, Cho <drajin.cho@gmail.com>
2022-07-16 19:46:08 +0900

    adding 겨울왕국2, 지뢰찾기

---
+++ 겨울왕국2/겨울왕국2.cpp
@@ -0,0 +1,136 @@
+﻿	#include <bangtal.h>
+
+#include <stdlib.h>
+
+SceneID scene;
+ObjectID game_board[16], init_board[16], start;
+
+const char* board_image[16] = {
+"Images/1.png",
+"Images/2.png",
+"Images/3.png",
+"Images/4.png",
+"Images/5.png",
+"Images/6.png",
+"Images/7.png",
+"Images/8.png",
+"Images/9.png",
+"Images/10.png",
+"Images/11.png",
+"Images/12.png",
+"Images/13.png",
+"Images/14.png",
+"Images/15.png",
+"Images/16.png",
+};
+int board_x[16] = { 300, 450, 600, 750, 300, 450, 600, 750, 300, 450, 600, 750, 300, 450, 600, 750 };
+int board_y[16] = { 470, 470, 470, 470, 320, 320, 320, 320, 170, 170, 170, 170, 20, 20, 20, 20 };
+int blank;
+
+TimerID timer;
+int mixCount;
+
+int board_index(ObjectID object) {
+	for (int i = 0; i < 16; i++) {
+		if (game_board[i] == object) return i;
+	}
+	return -1;
+}
+
+void board_move(int index) {
+	ObjectID t = game_board[blank];
+	game_board[blank] = game_board[index];
+	game_board[index] = t;
+
+	locateObject(game_board[index], scene, board_x[index], board_y[index]);
+	locateObject(game_board[blank], scene, board_x[blank], board_y[blank]);
+
+	blank = index;
+}
+
+bool movable(int index) {
+	if (index < 0 || index > 15) return false;
+	if (blank % 4 != 0 and index == blank - 1) return true;
+	if (blank % 4 != 3 and index == blank + 1) return true;
+	if (blank / 4 != 0 and index == blank - 4) return true;
+	if (blank / 4 != 3 and index == blank + 4) return true;
+	return false;
+}
+
+void board_mix() {
+	int index;
+
+	do {
+		switch (rand() % 4) {
+		case 0: index = blank - 1; break;
+		case 1: index = blank + 1; break;
+		case 2: index = blank - 4; break;
+		case 3: index = blank + 4; break;
+		}
+	} while (!movable(index));
+	
+	board_move(index);
+}
+
+bool completed() {
+	for (int i = 0; i < 16; i++) {
+		if (game_board[i] != init_board[i]) return false;
+	}
+	return true;
+}
+
+void mouseCallback(ObjectID object, int x, int y, MouseAction action) {
+	if (object == start) {
+		mixCount = 10;
+
+		setTimer(timer, 1.f);
+		startTimer(timer);
+	}
+	else {
+		int index = board_index(object);
+		if (movable(index)) {
+			board_move(index);
+
+			if (completed())
+				showMessage("Completed!!!");
+		}
+	}
+}
+
+void timerCallback(TimerID timer) {
+	mixCount--;
+	if (mixCount > 0) {
+		board_mix();
+
+		setTimer(timer, 1.f);
+		startTimer(timer);
+	}
+}
+
+int main() {
+	setMouseCallback(mouseCallback);
+	setTimerCallback(timerCallback);
+
+	scene = createScene("겨울왕국2", "Images/배경.png");
+
+	for (int i = 0; i < 16; i++) {
+		game_board[i] = createObject(board_image[i]);
+		init_board[i] = game_board[i];
+
+		locateObject(game_board[i], scene, board_x[i], board_y[i]);
+		showObject(game_board[i]);
+	}
+
+	blank = 15;
+	hideObject(game_board[blank]);
+
+	start = createObject("Images/start.png");
+	locateObject(start, scene, 540, 100);
+	showObject(start);
+
+	timer = createTimer(1.f);
+
+	startGame(scene);
+
+	return 0;
+}
---
+++ 지뢰찾기/지뢰찾기.cpp
@@ -0,0 +1,144 @@
+﻿#include <bangtal.h>
+
+#include <time.h>
+#include <stdlib.h>
+
+SceneID scene;
+
+const int BUTTON_WIDTH = 40;
+const int BUTTON_HEIGHT = 40;
+
+const int COLUMN = 20;
+const int ROW = 10;
+const int BOMBS = 25;
+
+int game[ROW][COLUMN];
+ObjectID board[ROW][COLUMN];
+bool opened[ROW][COLUMN];
+
+const char* buttonImage[9] = {
+	"Images/0.png",
+	"Images/1.png",
+	"Images/2.png",
+	"Images/3.png",
+	"Images/4.png",
+	"Images/5.png",
+	"Images/6.png",
+	"Images/7.png",
+	"Images/8.png",
+};
+
+ObjectID start;
+
+void initGame() {
+	for (int y = 0; y < ROW; ++y)
+		for (int x = 0; x < COLUMN; ++x) game[y][x] = 0;
+
+	for (int i = 0; i < BOMBS; ++i) {
+		int x, y;
+		do {
+			x = rand() % COLUMN;
+			y = rand() % ROW;
+		} while (game[y][x] != 0);
+		game[y][x] = -1;			// BOMB
+	}
+
+	for (int y = 0; y < ROW; ++y)
+		for (int x = 0; x < COLUMN; ++x) {
+			if (game[y][x] == 0) {
+				int count = 0;
+				for (int j = y - 1; j <= y + 1; ++j)
+					for (int k = x - 1; k <= x + 1; ++k) {
+						if (j >= 0 && j < ROW && k >= 0 && k < COLUMN)
+							if (game[j][k] == -1) ++count;
+					}
+				game[y][x] = count;
+			}
+
+			opened[y][x] = false;
+			setObjectImage(board[y][x], "Images/button.png");
+		}
+}
+
+void buttonToXY(ObjectID button, int& x, int& y) {
+	for (y = 0; y < ROW; ++y)
+		for (x = 0; x < COLUMN; ++x)
+			if (button == board[y][x]) return;
+}
+
+bool checkEnd() {
+	int count = 0;
+	for (int y = 0; y < ROW; ++y)
+		for (int x = 0; x < COLUMN; ++x)
+			if (opened[y][x] == false) ++count;
+
+	return count == BOMBS;
+}
+
+void openButton(int x, int y) {
+	if (opened[y][x] == false) {
+		if (game[y][x] < 0) {
+			setObjectImage(board[y][x], "Images/bomb.png");		// bomb
+		}
+		else setObjectImage(board[y][x], buttonImage[game[y][x]]);
+		opened[y][x] = true;
+
+		if (game[y][x] == 0) {
+			for (int j = y - 1; j <= y + 1; ++j)
+				for (int k = x - 1; k <= x + 1; ++k) {
+					if (j >= 0 && j < ROW && k >= 0 && k < COLUMN)
+						openButton(k, j);
+				}
+		}
+	}
+}
+
+void mouseCallback(ObjectID object, int x, int y, MouseAction action) {
+	if (object == start) {
+		initGame();
+	}
+	else {
+		buttonToXY(object, x, y);
+		openButton(x, y);
+
+		if (game[y][x] < 0) {
+			// open bomb
+			showMessage("game failed");
+		}
+		else if (checkEnd())
+			showMessage("game successful");
+	}
+}
+
+ObjectID createObject(const char* image, SceneID scene, int x, int y, bool shown = true) {
+	ObjectID object = createObject(image);
+	locateObject(object, scene, x, y);
+	if (shown) showObject(object);
+
+	return object;
+}
+
+int main() {
+	srand((unsigned int)time(NULL));
+
+	setGameOption(GameOption::GAME_OPTION_ROOM_TITLE, false);
+	setGameOption(GameOption::GAME_OPTION_INVENTORY_BUTTON, false);
+	setGameOption(GameOption::GAME_OPTION_MESSAGE_BOX_BUTTON, false);
+
+	setMouseCallback(mouseCallback);
+
+	scene = createScene("지뢰찾기");	
+
+	for (int y = 0; y < ROW; ++y)
+		for (int x = 0; x < COLUMN; ++x)	
+			board[y][x] = createObject("Images/button.png", scene, x * BUTTON_WIDTH, y * BUTTON_HEIGHT);
+
+	start = createObject("Images/start.png", scene, 1000, 300);
+
+	initGame();
+
+	startGame(scene);
+
+	return 0;
+}
+
